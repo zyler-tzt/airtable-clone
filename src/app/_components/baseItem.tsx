f4489f7a1bbbb6033e4073e2384c26890304e530
zyler-tzt <thantzin.tzh14@gmail.com>
2025-05-24 20:23:29 +1000

    Fix ui on base create

--- src/app/_components/baseItem.tsx
+++ src/app/_components/baseItem.tsx
@@ -1,13 +1,24 @@
 import type { Base } from '@prisma/client';
 import Image from 'next/image'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "~/app/_components/ui/dialog"
+import { api } from "~/trpc/react";
+
+import { Button } from "~/app/_components/ui/button"
+import { Input } from "~/app/_components/ui/input"
+import { Label } from "~/app/_components/ui/label"
+import { useState } from 'react';
 
 type BaseItemProps = {
   base: Base;
 };
-
-type CreateBaseItemProps = {
-    onClick: () => void;
-}
 
 export function BaseItem({ base }: BaseItemProps) {
     return (
@@ -33,16 +44,64 @@ export function BaseItem({ base }: BaseItemProps) {
     )
 }
 
-export function CreateBaseItem({ onClick }: CreateBaseItemProps) {
+export function CreateBaseItem() {
+    const [open, setOpen] = useState(false)
+    const [newBaseName, setNewBaseName] = useState("")
+    const utils = api.useUtils();
+
+
+    const createBase = api.base.create.useMutation({
+        onSuccess: async () => {
+            await utils.base.invalidate();
+            setOpen(false)
+        },
+    });
+
+    async function baseCreateHandler() {
+        const name = newBaseName.trim() === "" ? "Untitled Base" : newBaseName
+        await createBase.mutateAsync({ name }) 
+    }
+
     return (
-        <div className='flex items-center justify-center border-2 border-gray-300 rounded-lg h-50' onClick={onClick}>
-            <Image 
-                src="/add-lucide.svg"
-                alt="addBaseImage"
-                draggable={false}
-                width={40}         
-                height={40} 
-            />
-        </div>
+        <Dialog open={open} onOpenChange={setOpen}>
+            <DialogTrigger asChild>
+                <div className='flex items-center justify-center border-2 border-gray-300 rounded-lg h-50'>
+                    <Image 
+                        src="/add-lucide.svg"
+                        alt="addBaseImage"
+                        draggable={false}
+                        width={40}         
+                        height={40} 
+                    />
+                </div>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                <DialogTitle>Create a new base</DialogTitle>
+                <DialogDescription>
+                    Enter a name for your base
+                </DialogDescription>
+                </DialogHeader>
+                <div className="grid gap-4 py-4">
+                <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="name" className="text-right">
+                    Name
+                    </Label>
+                    <Input
+                    id="name"
+                    className="col-span-3"
+                    onChange={(e) => setNewBaseName(e.target.value)}
+                    disabled={createBase.isPending}
+                    />
+                </div>
+                </div>
+                <DialogFooter>
+                <Button onClick={baseCreateHandler} disabled={createBase.isPending}
+>
+                    Create Base
+                </Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
     )
 }
